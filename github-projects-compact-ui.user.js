44de6e0fae017f59a023204cffa4257ad7ce6be9
Bryan de Jong <bryandejong99@gmail.com>
2023-08-23 15:11:44 +0100

    feat: add github-projects-compact-ui.user.js

---
+++ github-projects-compact-ui.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Github Projects Compact UI
+// @namespace    https://github.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       Bryan de Jong
+// @match        *://github.com/users/*/projects/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=userscript.zone
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+
+    'use strict';
+
+    GM_addStyle('.AppHeader .AppHeader-globalBar { padding: 4px !important; }');
+
+})();
